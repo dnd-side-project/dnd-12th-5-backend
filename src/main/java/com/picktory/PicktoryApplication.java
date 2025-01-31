eb184e2e1ee6b5ad31c18940f3772ac723e301ae
TaskerJang <bymyself103@naver.com>
2025-01-31 19:22:29 +0900

    기본 버전 배포

---
+++ src/main/java/com/picktory/PicktoryApplication.java
@@ -0,0 +1,11 @@
+package com.picktory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicktoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PicktoryApplication.class, args);
+    }
+} 
