b69f45b5a688509af932bc2102378b6b39dc19d3
TaskerJang <bymyself103@naver.com>
2025-01-31 20:36:27 +0900

    SWAGGER 설정 완료

---
+++ src/main/java/com/picktory/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.picktory.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("PickTory API")
+                .version("1.0.0")
+                .description("PickTory 백엔드 API 문서");
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme));
+    }
+}
