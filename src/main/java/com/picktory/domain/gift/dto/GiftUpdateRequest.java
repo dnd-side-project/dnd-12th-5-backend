d0229fb48e66107eefdb88a3dc790000ddeb9b4c
kitechoi <chevel0212@gmail.com>
2025-02-11 15:30:55 +0900

    [refactor]: 보따리 업데이트(선물 수정,추가,삭제) 로직 수정

--- src/main/java/com/picktory/domain/gift/dto/GiftUpdateRequest.java
+++ src/main/java/com/picktory/domain/gift/dto/GiftUpdateRequest.java
@@ -1,11 +1,15 @@
 package com.picktory.domain.gift.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GiftUpdateRequest implements GiftImageRequest {
     private Long id;
 
