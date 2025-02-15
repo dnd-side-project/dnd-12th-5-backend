f65b22be1a8b2e49a76ec8a5bbd8e2b626871037
TaskerJang <bymyself103@naver.com>
2025-02-15 21:11:19 +0900

    [feat]: ResponseRepository에 답변 존재 여부 확인 메서드 추가

--- src/main/java/com/picktory/domain/response/repository/ResponseRepository.java
+++ src/main/java/com/picktory/domain/response/repository/ResponseRepository.java
@@ -3,6 +3,9 @@ package com.picktory.domain.response.repository;
 import com.picktory.domain.response.entity.Response;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ResponseRepository extends JpaRepository<Response, Long>, ResponseRepositoryCustom {
     boolean existsByGiftId(Long giftId);
+    boolean existsByGiftIdIn(List<Long> giftIds);
 }
