4cacb8ade83a229adf28a59105c155a268a67adf
TaskerJang <bymyself103@naver.com>
2025-04-22 13:05:47 +0900

    [test]: 답변저장 테스트 코드 수정

--- src/main/java/com/picktory/domain/gift/enums/GiftResponseTag.java
+++ src/main/java/com/picktory/domain/gift/enums/GiftResponseTag.java
@@ -1,9 +1,10 @@
 package com.picktory.domain.gift.enums;
 
 public enum GiftResponseTag {
-    GREAT,
-    GOOD,
-    ALREADY_HAVE,
-    NOT_SURE,
-    NOT_MY_STYLE
+    GREAT,     // "갖고 싶던 선물이에요"
+    GOOD,      // "정말 최고에요"
+    ALREADY_HAVE, // "이미 가지고 있어요"
+    NOT_SURE,  // "잘 모르겠어요"
+    NOT_MY_STYLE, // "제 취향이 아니에요"
+    LIKE       // "마음에 들어요"
 }
